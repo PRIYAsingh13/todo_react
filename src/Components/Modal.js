8324cd3289d614c6c5e3433e112656a5707c7a13
PRIYAsingh13 <ps597934@gmail.com>
2023-02-22 23:38:46 +0530

    todo

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,6 +1,6 @@
 import React from "react";
-import './Modal.css';
-import { TextField, Button } from "@mui/material";
+import "./Modal.css";
+import { TextField, Button, InputLabel, Select, MenuItem,FormControl } from "@mui/material";
 
 const Modal = () => {
   return (
@@ -13,10 +13,27 @@ const Modal = () => {
         multiline
         maxRows={4}
       />
+     
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Status</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          // value={age}
+          label="Age"
+          // onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      <div className="all-btn">
       <Button variant="contained">Add</Button>
       <Button variant="outlined" color="error">
         close
       </Button>
+    </div>
     </div>
   );
 };
